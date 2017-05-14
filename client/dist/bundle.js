9ddf264311712a0ab86c51f787e86f4ba6b48e4d
Nguyen Dang Khoa <dangkhoaicao@gmail.com>
2017-05-14 23:38:21 +0700

    change api to heroku

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -11267,7 +11267,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var API = {
-    url_food_list: 'http://localhost:8000/foodlist'
+    url_food_list: 'https://petzone.herokuapp.com/foodlist'
 };
 
 var CONST = {
--- client/src/jsx/containers/FoodView.jsx
+++ client/src/jsx/containers/FoodView.jsx
@@ -8,7 +8,7 @@ import TEXT from 'ultils/lang';
 
 
 const API = {
-    url_food_list: 'http://localhost:8000/foodlist'
+    url_food_list: 'https://petzone.herokuapp.com/foodlist'
 }
 
 
