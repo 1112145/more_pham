02301faea3d56e2f0886f539062b17355fddd938
Nguyen Dang Khoa <dangkhoaicao@gmail.com>
2017-05-14 23:35:03 +0700

    commit

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ var db = require('./database/db.js');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 8000));
+
 var PATH_CLIENT_FOLDER = path.resolve(__dirname, './client/');
 app.use(express.static(path.resolve(PATH_CLIENT_FOLDER, 'dist')));
 
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
     res.sendFile(PATH_CLIENT_FOLDER + '/dist/index.html');
 });
 
 app.get('/foodlist', function (req, res) {
-    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
     res.send(JSON.stringify(db.getFoodList()));
 });
 
 
-var http = app.listen(8000, function (req, res) {
+var http = app.listen(app.get('port'), function (req, res) {
     console.log('Server started at port: ' + 8000);
 });
